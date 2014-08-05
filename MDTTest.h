259e44e96d897b4875b46d43d99e9d04b9db04c8
MICHAEL TITMUS <michaeltitmus@MICHAELs-iMac.local>
2014-08-05 15:15:02 -0400

    added a test class

---
+++ MDTTest.h
@@ -0,0 +1,13 @@
+//
+//  MDTTest.h
+//  GIT Test
+//
+//  Created by MICHAEL TITMUS on 8/5/14.
+//  Copyright (c) 2014 MDT & Associates, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MDTTest : NSObject
+
+@end
---
+++ MDTTest.m
@@ -0,0 +1,13 @@
+//
+//  MDTTest.m
+//  GIT Test
+//
+//  Created by MICHAEL TITMUS on 8/5/14.
+//  Copyright (c) 2014 MDT & Associates, LLC. All rights reserved.
+//
+
+#import "MDTTest.h"
+
+@implementation MDTTest
+
+@end
